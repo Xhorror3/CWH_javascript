29b6d84456e706ec27d93f03d086e305d49e1192
Shree <buchadeshree33@gmail.com>
2023-09-17 13:19:13 +0530

    Datatypes(let,var,const)

---
+++ 03_var_let_const.js
@@ -0,0 +1,10 @@
+var a=1;
+var a=9;
+let b=10;
+// let b=90; //throws an error 
+b=100;  //this is valid
+console.log(b)
+console.log(a)
+// const ab=100;
+// let ab=09; //throws an error
+// ab=90;  //throws an error
