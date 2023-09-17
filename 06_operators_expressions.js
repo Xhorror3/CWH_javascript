fbbfa8339f2dd774ca132dd008598da995df2792
Shree <buchadeshree33@gmail.com>
2023-09-17 13:20:17 +0530

    Operators and expressions

---
+++ 06_operators_expressions.js
@@ -0,0 +1,46 @@
+// operators in JS
+// a=10;
+// b=4;
+// // Arithmatic operators
+// console.log("a+b =",a+b)
+// console.log("a-b =",a-b)
+// console.log("a/b =",a/b)
+// console.log("a*b =",a*b)
+// console.log("a%b =",a%b)
+// console.log("a**b =",a**b)
+// console.log("a++ =",a++)
+// console.log("++a =",++a)
+// console.log("a-- =",a--)
+// console.log("--a =",--a)
+
+// assignment operators
+// let x=1;
+// x+=1;
+// console.log("x is",x)
+// x-=2;
+// console.log("x is",x)
+// x=9;
+// x*=2;
+// console.log("x is",x)
+// x/=6;
+// console.log("x is",x)
+// x**=2;
+// console.log("x is",x)
+
+// comparision operators
+// let comp1=3;
+// let comp2=0;
+// let comp3="3";
+// console.log(comp1==comp2)
+// console.log(comp1!=comp2)
+// console.log(comp1===comp3)
+// console.log(comp1!==comp3)
+// console.log(comp1>=comp2)
+// console.log(comp1<=comp2)
+
+// logical operators
+let r=1;
+let m=2;
+console.log(r==1 && m==2)
+console.log(r!=1 || m!=2)
+console.log(!true)
