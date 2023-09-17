49b6d20a97950ac9ba110812c059668364c6dcc9
Shree <buchadeshree33@gmail.com>
2023-09-17 13:19:47 +0530

    Problem set 1

---
+++ 04_primitive_objects.js
@@ -0,0 +1,18 @@
+// primitive datatypes
+let a=null;
+let b=34;
+let c=true;
+let d=BigInt(45);
+let e="Hello";
+let f=Symbol("Hey");
+let g=undefined;
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+
+// objects
+const item={
+  "harry" : true,
+  "rohan":67,
+  "poop": undefined,
+}
+console.log(item["jj"])
