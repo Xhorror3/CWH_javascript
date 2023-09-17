30d54599fe469cdd04811d698829a8563f6d4f6e
Shree <buchadeshree33@gmail.com>
2023-09-17 13:23:00 +0530

    Problem set 1 by mistakely 04 is assigned with this title and title of 04 is Objects and primitives

---
+++ 05_chapter1_ps.js
@@ -0,0 +1,22 @@
+// Q1
+let a="Harry"
+let b=65
+console.log(a+b)
+
+// Q2
+console.log(typeof (a+b))
+
+// Q3
+const dict={
+  happy:"Being happy",
+  sad:"Being sad",
+  fat:"Panny"
+}
+// dict=67;  //not possible
+
+// Q4
+dict["dame"]="Boss"
+console.log(dict)
+
+//Q5
+console.log(dict.sad)
