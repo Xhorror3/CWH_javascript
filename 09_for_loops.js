1f6f16e18c03810f2c2faa307f520c2f25c25402
Shree <buchadeshree33@gmail.com>
2023-09-21 13:36:32 +0530

    For loops

---
+++ 09_for_loops.js
@@ -0,0 +1,28 @@
+// for loop
+/*
+let sum=0
+let n=prompt("Enter the number:")
+n=Number.parseInt(n)
+for(let i=1; i<=n;i++)
+  {
+    sum+=i
+  }
+console.log("The sum of "+n+" numbers is "+sum)
+*/
+
+// for in loop
+/*
+let obj={
+  harry:20,
+  shubham:90,
+  harsh:49,
+}
+for (let key in obj){
+  console.log("Marks of "+key+" are "+obj[key])
+}
+*/
+
+// for of loop
+for(let a of "Harry"){      //here Harry should be itreable
+    console.log(a)
+  }
