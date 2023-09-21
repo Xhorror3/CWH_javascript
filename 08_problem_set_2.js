ddd5011d3d60ba756aa7db7ca044f1259d26ff42
Shree <buchadeshree33@gmail.com>
2023-09-21 13:36:04 +0530

    Problem set 2

---
+++ 08_problem_set_2.js
@@ -0,0 +1,50 @@
+let age=prompt("What is your age?")
+age =Number.parseInt(age)
+
+// Q1]
+
+// if (age>10 && age <=20){
+//   alert("Your age lies in between 10 and 20.")
+// }
+// else{
+//   alert("Your age is less than 10 or greater than 20.")
+// }
+
+// Q2]
+// switch(age){
+//   case 12:
+//     alert("Your age is 12.")
+//     break
+//   case 13:
+//     alert("Your age is 13.")
+//     break
+//   case 14:
+//     alert("Your age is 14.")
+//     break
+//   case 15:
+//     alert("Your age is 15.")
+//     break
+//   default:
+//     alert("Your age is not special.")
+// }
+
+// Q3
+// if(age%2==0 && age %3==0)
+// {
+//   console.log("Your age is divisible by 2 and 3.")
+// }
+// else{
+//   console.log("Your age is not divisible by 2 and 3.")
+// }
+
+// Q4
+// if(age%2==0 || age %3==0)
+// {
+//   console.log("Your age is divisible by 2 or 3.")
+// }
+// else{
+//   console.log("Your age is not divisible by 2 or 3.")
+// }
+
+// Q5
+console.log(age>18? "You can drive":"You cannot drive")
