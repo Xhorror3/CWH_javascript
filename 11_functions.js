3f31b32003fa6a3e41bcf0510dca9a41fec48eb3
Shree <buchadeshree33@gmail.com>
2023-09-25 19:15:37 +0530

    Functions

---
+++ 11_functions.js
@@ -0,0 +1,17 @@
+// these are two methods of declaring the fuctions
+const avgOfNumbers=(x,y)=>{
+    return (x+y)/2
+  }
+  
+  function OnePlusavgNum(x,y){
+    return 1+(x+y)/2
+  }
+  const hello=()=>{
+    console.log("Hello")
+  }
+  
+  let a=9
+  let b=8
+  hello();
+  console.log(avgOfNumbers(a,b))
+  console.log(OnePlusavgNum(a,b))
