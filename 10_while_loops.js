cc456e73e4b798e232e3ce780d8dba6fb9a08c1a
Shree <buchadeshree33@gmail.com>
2023-09-25 19:15:03 +0530

    While loops

---
+++ 10_while_loops.js
@@ -0,0 +1,19 @@
+const prompt=require('prompt-sync')();
+
+let a=prompt("Enter the number:");
+a=Number.parseInt(a)
+
+// // while loop
+
+// let i=0;
+// while(i<a){
+//     console.log(i)
+//     i++
+// }
+
+// do while loop
+
+let i=1
+do{
+    console.log(i)
+}while(false)
