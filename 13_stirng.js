ca4bc0603bc7ec251a99ad9bd31586fb8262d053
Shree <buchadeshree33@gmail.com>
2023-09-30 09:58:10 +0530

    String Introduction

---
+++ 13_stirng.js
@@ -0,0 +1,11 @@
+let string="harry"
+let friend='Harry'
+// console.log(string,friend)
+
+// console.log(string.length)
+
+// console.log(`My name is ${string} and my titlecase is ${friend}`)
+
+// escape sequence
+// console.log('who's your boss')   this will give error
+console.log('who\'s your boss')  // here  \' is used as escape sequence
