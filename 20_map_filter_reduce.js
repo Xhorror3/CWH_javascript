b8b65746f299e8f237a13b8d51c344a5bd427ecb
Shree <buchadeshree33@gmail.com>
2023-10-03 12:34:53 +0530

    Map,filter,reduce methods

---
+++ 20_map_filter_reduce.js
@@ -0,0 +1,34 @@
+// map method
+// let arr1=[12,3,45,6]
+// let a=arr1.map((value)=>{
+//   console.log(value)
+//   return value+1
+// })
+// console.log(a)
+
+// let a=arr1.map((value,index,arr)=>{
+//   console.log(value,index,arr)
+//   return value+1
+// })
+// console.log(a)
+
+// filter method
+// let arr2=[1,2,34,5,3,67]
+// let b=arr2.filter((value)=>{
+//   return value<10
+// })
+// console.log(b,arr2)
+
+// reduce method
+let arr3=[1,2,4]
+// let b=arr3.reduce((v1,v2)=>{
+//   return v1*v2
+// })
+// console.log(b)
+
+// above function can be written like below
+// let reduce_value=(v1,v2)=>{
+//   return v1*v2
+// }
+// let b=arr3.reduce(reduce_value)
+// console.log(b)
