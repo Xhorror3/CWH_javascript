c0044627d57dd091b37800b32d8b0238558f8249
Shree <buchadeshree33@gmail.com>
2023-10-03 12:34:10 +0530

    Array loops

---
+++ 19_array_loops.js
@@ -0,0 +1,27 @@
+let num=[9,3,2,1,5,32]
+
+// for loop
+// for(let i=0;i<num.length;i++){
+//   console.log(num[i])
+// }
+
+// for each loop
+// num.forEach((key)=>{
+//   console.log(key*key)
+// })
+
+// Array.from
+// let str="Humpty"
+// let arr=Array.from(str)
+// console.log(arr)
+
+// for..of
+// for(let i of num){
+//   console.log(i)
+// }
+
+// for ..in
+// for (let i in num){
+//   console.log(i)
+//   console.log(num[i])
+// }
